a513fc6da712ebdfd6bdfd5dbef13227ce2b1c86
gracelove <govlmo91@gmail.com>
2019-11-08 14:01:59 +0900

    Create ExamController.java

---
+++ src/main/java/com/petcare/web/controller/ExamController.java
@@ -0,0 +1,16 @@
+package com.petcare.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exam")
+public class ExamController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "홍창용님 안녕하세요.";
+    }
+}
